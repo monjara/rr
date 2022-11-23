5a4866a530d89f3cdcdb2fb9dc73b9a9998f7495
monjara <yaginumee@gmail.com>
2022-11-23 13:44:48 +0900

    fix setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useNavigate, Routes, Route } from 'react-router-dom'
 
-function App() {
+const Hello = () => {
+  const navigate = useNavigate();
   return (
-    <p>sample</p>
+    <>
+      <p>Hello</p>
+      <button onClick={() => navigate("/news")}>News</button>
+      <button onClick={() => navigate("/about")}>About</button>
+    </>
+  );
+};
+
+const News = () => {
+  return <h1>News</h1>;
+};
+
+const About = () => {
+  return <h1>About</h1>;
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Hello />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/news" element={<News />} />
+    </Routes>
   )
 }
 
