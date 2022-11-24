004d8e6bd4e660185d144fb89c5657c2c6446736
monjara <yaginumee@gmail.com>
2022-11-24 21:37:17 +0900

    fix structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
 import React from 'react'
-import { useNavigate, Routes, Route } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
-const Hello = () => {
-  const navigate = useNavigate();
-  return (
-    <>
-      <p>Hello</p>
-      <button onClick={() => navigate("/news")}>News</button>
-      <button onClick={() => navigate("/about")}>About</button>
-    </>
-  );
-};
-
-const News = () => {
-  return <h1>News</h1>;
-};
-
-const About = () => {
-  return <h1>About</h1>;
-};
-
-const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Hello />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/news" element={<News />} />
-    </Routes>
-  )
-}
-
-export default App
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<p>asdf</p>} />
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>
+)
 
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite';
 
-
 export default defineConfig({
   plugins: [react()],
   root: 'src',
+  server: {
+    port: 8080,
+    host: true
+  },
   build: {
     outDir: 'build'
-  }
+  },
+  envDir: '../'
 })
-
 
