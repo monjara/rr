7ab38307046e1d95c3d9e2102eeda74ca2e253e2
monjara <yaginumee@gmail.com>
2022-11-30 19:39:16 +0900

    add setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter, Route, Routes } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path='/' element={<p>asdf</p>} />
-      </Routes>
-    </HashRouter>
+    <ChakraProvider>
+      <RecoilRoot>
+        <HashRouter>
+          <Routes>
+            <Route path='/' element={<p>asdfsdf</p>} />
+          </Routes>
+        </HashRouter>
+      </RecoilRoot>
+    </ChakraProvider>
   </React.StrictMode>
 )
 
