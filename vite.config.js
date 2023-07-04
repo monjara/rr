59877e2e9184fa5c0d39867a03ad36e37fab188b
mo <yaginumtt@gmail.com>
2023-07-04 22:09:24 +0900

    add library

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxImportSource: '@emotion/react'
+  })],
   root: 'src',
   server: {
     port: 8080,
