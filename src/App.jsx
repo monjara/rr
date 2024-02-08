66f525484f22913135b282eb6ce14b8cf4b4d68a
monjara <yaginumee@gmail.com>
2024-02-08 18:04:41 +0900

    resolve Router error

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
-  Route,
   RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements
+  createBrowserRouter
 } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
 
 import RootRoutes from './features/root/RootRoutes'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<RootRoutes />} />
-  )
-)
+const router = createBrowserRouter([
+  {
+    path: '*',
+    element: <RootRoutes />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
